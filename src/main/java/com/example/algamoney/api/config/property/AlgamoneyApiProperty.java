831bd916bffb2c14ba3e45acd6bee8b875d05931
Fabio Nascimento <fabiogtr@hotmail.com>
2021-11-06 08:31:46 -0300

    Enviando um e-mail simples

--- src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -3,13 +3,18 @@ package com.example.algamoney.api.config.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
 	private String originPermitida = "http://localhost:8000";
 	
 	private final Seguranca seguranca = new Seguranca();
+	
+	private final Mail mail = new Mail();
+	
+	public Mail getMail() {
+		return mail;
+	}
 	
 	public String getOriginPermitida() {
 		return originPermitida;
@@ -33,6 +38,46 @@ public class AlgamoneyApiProperty {
 
 		public void setEnableHttps(boolean enableHttps) {
 			this.enableHttps = enableHttps;
+		}
+	}
+	
+	public static class Mail {
+		
+		private String host;
+		private Integer port;
+		private String username;
+		private String password;
+		
+		public String getHost() {
+			return host;
+		}
+		
+		public void setHost(String host) {
+			this.host = host;
+		}
+		
+		public Integer getPort() {
+			return port;
+		}
+		
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		
+		public String getPassword() {
+			return password;
+		}
+		
+		public void setPassword(String password) {
+			this.password = password;
 		}
 	}
 }
