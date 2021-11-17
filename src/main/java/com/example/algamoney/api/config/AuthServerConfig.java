e5e197bd9a3e45a461e160208c706d6875f9c0fd
Fabio Nascimento <fabiogtr@hotmail.com>
2021-11-17 16:29:34 -0300

    Gerando chave RSA

--- src/main/java/com/example/algamoney/api/config/AuthServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.config;
 
+import java.io.File;
+import java.security.KeyStore;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,11 +37,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 import com.example.algamoney.api.security.UsuarioSistema;
-import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
@@ -123,13 +123,16 @@ public class AuthServerConfig {
 	}
 	
 	@Bean
-	public JWKSet jwkSet() throws JOSEException {
-		RSAKey rsa = new RSAKeyGenerator(2048)
-				.keyUse(KeyUse.SIGNATURE)
-				.keyID(UUID.randomUUID().toString())
-				.generate();
+	public JWKSet jwkSet() throws Exception {
+		File file = new ClassPathResource("keystore/algamoney.jks").getFile();
 		
-		return new JWKSet(rsa);
+		KeyStore keyStore  = KeyStore.Builder.newInstance(file,
+				new KeyStore.PasswordProtection("123456".toCharArray())
+				).getKeyStore();
+		
+		RSAKey rsaKey = RSAKey.load(keyStore, "algamoney", "123456".toCharArray());
+		
+		return new JWKSet(rsaKey);
 	}
 	
 	@Bean
